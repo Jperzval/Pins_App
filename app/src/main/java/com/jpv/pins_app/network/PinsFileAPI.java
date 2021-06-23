ffe0fb9392d2156b835c3a0373a019755168657f
Jperzval <perezvalle.jes@gmail.com>
2021-06-23 19:33:21 -0400

    created network interface

---
+++ app/src/main/java/com/jpv/pins_app/network/PinsFileAPI.java
@@ -0,0 +1,12 @@
+package com.jpv.pins_app.network;
+
+import com.jpv.pins_app.data.PinsWrapper;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.GET;
+
+public interface PinsFileAPI {
+
+    @GET("Jperzval/Pins_App/dev-jess/app/src/main/res/raw/nyc_ttp_pins.json")
+    Observable<PinsWrapper> getPinsWrapper();
+}
